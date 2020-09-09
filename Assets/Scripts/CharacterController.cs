0a4e05703592ff7f060cb361e9a78a5a7972ca11
AmrMHady92 <amrmhady92@gmail.com>
2020-09-09 11:05:39 +0200

    Character controller and Memory puller

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour
+{
+
+    public float speed = 2;
+    public bool xOnly = false;
+    public Vector2 outsideForce = Vector2.zero;
+    private Rigidbody2D rb;
+    bool active = true;
+    [HideInInspector] public float xDir, yDir = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (!active) return;
+
+        xDir = 0;
+        yDir = 0;
+
+        if (Input.GetKey(KeyCode.W) && !xOnly)
+        {
+            yDir = 1;
+        }
+        if (Input.GetKey(KeyCode.S) && !xOnly)
+        {
+            yDir = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            xDir = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            xDir = -1;
+        }
+
+        rb.velocity = new Vector2(xDir * speed, yDir * speed) + outsideForce;
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/MemoryPuller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryPuller : MonoBehaviour
+{
+    public float forcePower = 2;
+    CharacterController player;
+    bool pulling = false;
+    float distance;
+    Vector2 direction;
+
+    private void Update()
+    {
+        if (pulling)
+        {
+            distance = Vector2.Distance(this.transform.position, player.transform.position) + 0.05f;
+            if(distance > 0.1f)
+            {
+                direction = this.transform.position - player.transform.position;
+                player.outsideForce = (direction.normalized * forcePower / distance);
+            }
+            else
+            {
+                player.outsideForce = Vector2.zero;
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<CharacterController>() != null)
+        {
+            Debug.Log("Enter");
+
+            if (player == null) player = collision.GetComponent<CharacterController>();
+            if (player == null)
+            {
+                Debug.LogError("No Rigid Body");
+                return;
+            }
+            pulling = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<CharacterController>() != null)
+        {
+            Debug.Log("Exit");
+            if (player == null) player = collision.GetComponent<CharacterController>();
+            if (player == null)
+            {
+                Debug.LogError("No Rigid Body");
+                return;
+            }
+            player.outsideForce = Vector2.zero;
+            pulling = false;
+        }
+    }
+
+}
